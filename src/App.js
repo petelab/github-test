5666daa5bb1fbb3377802e0d8b673c6c86501757
randomer2 <petelab@users.noreply.github.com>
2021-09-03 11:14:44 +0000

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useState,useEffect} from "react"
 
-const mySecret = process.env["testkey"];
+
 
 function App() {
+  const [value,setValue] = useState(()=>{
+    return localStorage.getItem("lorem") || ""
+  })
+  const [loading,setLoading] = useState(false)
   const handleGet = async () => {
-    const response = await fetch(`https://baconipsum.com/api/?${mySecret}`);
-    console.log(response);
+    setLoading(true)
+    const response = await fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=1`);
+    const data = await response.json();
+    const [first] = data
+    
+    setValue(first)
+    console.log(first);
+    setLoading(false)
   };
+  const handleClear = ()=>{
+    setValue("")
+  }
+  useEffect(()=>{
+    localStorage.setItem("lorem",value)
+  },[value])
   return (
     <div className="App">
       <h1>Hello there</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-        repudiandae consequatur optio dicta enim, quibusdam ad, est accusantium
-        veritatis iste reprehenderit id 111.
-      </p>
-
-      <button onClick={handleGet}>click</button>
+      <div className="box">
+      {value !== "" ? <p>{value}</p>: <p>no value</p>}
+</div>
+      <button onClick={handleGet} disabled={loading}>click</button>
+      <button onClick={handleClear}>Clear Local Storage</button>
     </div>
   );
 }
