747ad7de60190e1abebde84d9906805f68fc051a
unknown <C:\Users\Peter\AppData\Roaming\The Bat!>
2021-09-03 10:50:03 +0200

    local test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello there</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        repudiandae consequatur optio dicta enim, quibusdam ad, est accusantium
+        veritatis iste reprehenderit id cum.
+      </p>
     </div>
   );
 }
