29695ad5a5ae0a009094641d278e1f915a310519
unknown <C:\Users\Peter\AppData\Roaming\The Bat!>
2021-09-03 11:06:10 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const mySecret = process.env['testkey']
+const mySecret = process.env["testkey"];
 
 function App() {
-
-  const handleGet = async ()=>{
+  const handleGet = async () => {
     const response = await fetch(`https://baconipsum.com/api/?${mySecret}`);
-    console.log(response)
-
-  }
+    console.log(response);
+  };
   return (
     <div className="App">
       <h1>Hello there</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
         repudiandae consequatur optio dicta enim, quibusdam ad, est accusantium
-        veritatis iste reprehenderit id cum.
+        veritatis iste reprehenderit id 111.
       </p>
       <button onClick={handleGet}>click</button>
     </div>
