79aa8ee5380bafb184a21db25dec12ba7a9b53c0
petelab <petelab@users.noreply.github.com>
2021-09-04 08:25:34 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,63 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {useState,useEffect} from "react"
-import {BrowserRouter,Route,Switch,Link} from "react-router-dom"
-
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 function App() {
-  const [value,setValue] = useState(()=>{
-    return localStorage.getItem("lorem") || ""
-  })
-  const [loading,setLoading] = useState(false)
+  const [value, setValue] = useState(() => {
+    return localStorage.getItem("lorem") || "";
+  });
+  const [loading, setLoading] = useState(false);
   const handleGet = async () => {
-    setLoading(true)
-    const response = await fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=1`);
+    setLoading(true);
+    const response = await fetch(
+      `https://baconipsum.com/api/?type=meat-and-filler&paras=1`
+    );
     const data = await response.json();
-    const [first] = data
-    
-    setValue(first)
+    const [first] = data;
+
+    setValue(first);
     console.log(first);
-    setLoading(false)
+    setLoading(false);
   };
-  const handleClear = ()=>{
-    setValue("")
-  }
-  useEffect(()=>{
-    localStorage.setItem("lorem",value)
-  },[value])
+  const handleClear = () => {
+    setValue("");
+  };
+  useEffect(() => {
+    localStorage.setItem("lorem", value);
+  }, [value]);
   return (
     <BrowserRouter>
-    <div className="App">
-      <Switch>
-<Route exact path="/">
- <h1>Hello there</h1>
-      <div className="box">
-      {value !== "" ? <p>{value}</p>: <p>no value</p>}
-</div>
-      <button onClick={handleGet} disabled={loading}>click</button>
-      <button onClick={handleClear}>Clear Local Storage</button>
-</Route>
-    </Switch>
-    </div>
+      <div className="App">
+        <nav>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/items">Items</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <h1>Hello there</h1>
+            <div className="box">
+              {value !== "" ? <p>{value}</p> : <p>no value</p>}
+            </div>
+            <button onClick={handleGet} disabled={loading}>
+              click
+            </button>
+            <button onClick={handleClear}>Clear Local Storage</button>
+          </Route>
+          <Route path="/about">
+            <h1>About Page</h1>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
